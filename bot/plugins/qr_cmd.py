d14fcbf3bcc2467f5b623b2f2c6ce1f94f9ad85b
HB <85717438+HBAMAL@users.noreply.github.com>
2021-07-31 18:28:49 +0530

    Update qr_cmd.py

--- bot/plugins/qr_cmd.py
+++ bot/plugins/qr_cmd.py
@@ -74,13 +74,16 @@ async def cb_data(bot, update):
         )
     else:
         await update.message.delete()
-@Client.on_message(filters.command("start"))
-async def start_message(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
-        reply_to_message_id=message.message_id,
-    )
+        
+@Client.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )     
 
 @Client.on_message(filters.command(["help", "h"]))
 async def help_message(client, message):
